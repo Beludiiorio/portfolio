c2002e60c83c69939e0dbf8d9bba1d1dcb91e7f5
Beludiiorio <beludiiorio@gmail.com>
2022-04-21 19:20:47 -0300

    cambios

--- portfolio/index.php
+++ portfolio/index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="css/Bootstrap/bootstrap/css/bootstrap.min.css">
     <script src="css/Bootstrap/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <title> Inicio </title>
+    <title>Inicio </title>
 </head>
 
 <body id="Inicio">
@@ -37,7 +37,7 @@
                         </li>
                     </ul>
                     <div class="d-flex">
-                        <a class="btn btn-rojo"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV</a>
+                        <a class="btn btn-rojo"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV </a>
                     </div>
                 </div>
             </div>
@@ -52,7 +52,7 @@
         <div class="row">
             <div class="col-12 col-sm-6 my-4 mt-sm-5 text center offset-sm-3 mb-3">
                 <div class="bienvenida pt-2"> 
-                     <p class="p-1"> Bienvenid@ a mi sitio web sobre docencia en sistemas.</p> 
+                     <p class="p-1"> Bienvenid@ a mi sitio web.</p> 
                 </div>
             </div>
         </div>
