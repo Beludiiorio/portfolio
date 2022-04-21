56948678e851b2bab9ab34b1d16c5c97c0de92e4
Beludiiorio <beludiiorio@gmail.com>
2022-04-21 19:34:16 -0300

    cambios

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <link rel="stylesheet" href="css/Bootstrap/bootstrap/css/bootstrap.min.css">
     <script src="css/Bootstrap/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <title> Inicio </title>
+    <title>Inicio </title>
 </head>
 
-<body id="Inicio ">
+<body id="Inicio">
     <header class="container">
         <nav class="navbar navbar-expand-md mb=4">
             <div class="container-fluid">
@@ -37,7 +37,7 @@
                         </li>
                     </ul>
                     <div class="d-flex">
-                        <a class="btn btn-rojo"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV</a>
+                        <a class="btn btn-rojo"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV </a>
                     </div>
                 </div>
             </div>
@@ -52,13 +52,13 @@
         <div class="row">
             <div class="col-12 col-sm-6 my-4 mt-sm-5 text center offset-sm-3 mb-3">
                 <div class="bienvenida pt-2"> 
-                     <p class="p-1"> Bienvenid@ a mi sitio web sobre docencia en sistemas.</p> 
+                     <p class="p-1"> Bienvenid@ a mi sitio web.</p> 
                 </div>
             </div>
         </div>
         <div class="row">
             <div class=" mis-proyectos col-12">
-                <a href="proyectos.php" class="btn shadow"> Conoce mis proyectos</a>
+                <a href="proyectos.php" class="btn shadow"> Conoce mis proyectos </a>
             </div>
         </div>
     </main>
